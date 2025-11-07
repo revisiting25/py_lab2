ac282c89c4331242ca248ececcee86c3b023bd88
Ina Sandbacka <sandbacka.ina@gmail.com>
2025-11-07 20:50:39 +0100

    add error handling for Shape

--- shape.py
+++ shape.py
@@ -1,5 +1,7 @@
 class Shape:
     def __init__(self, x = 0, y = 0):
+        if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+            raise TypeError("Center x and y must be numerical values (int or float).")
 
         self._x = x
         self._y = y
@@ -12,5 +14,7 @@ class Shape:
 
 
     def translate(self, dx, dy):
+        if not (isinstance(dx, (int, float)) and isinstance(dy, (int, float))):
+            raise TypeError("Translation dx and dy must be numerical values (int or float).")
         self._x += dx
         self._y += dy
